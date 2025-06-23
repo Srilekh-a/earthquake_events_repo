8fc69ede8965334645666d4085eaa5ebfcb54a73
Ananthoju Srilekha <srilekhaananthoju@gmail.com>
2025-06-23 12:50:57 +0530

    Committing 7 items from workspace 65ab5960-058e-41ab-a54f-de1fa91231e1

---
+++ fabric_items/02 Worldwide Earthquake Events API - Silver Layer Processing.Notebook/notebook-content.py
@@ -0,0 +1,103 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "f342b043-6fdb-4bda-996a-e615c89a2e5d",
+# META       "default_lakehouse_name": "Earthquake_lakehouse",
+# META       "default_lakehouse_workspace_id": "65ab5960-058e-41ab-a54f-de1fa91231e1",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "f342b043-6fdb-4bda-996a-e615c89a2e5d"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# # Worldwide Earthquake Events API - Silver Layer Processing
+
+# CELL ********************
+
+from pyspark.sql.functions import col
+from pyspark.sql.types import TimestampType
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# df now is a Spark DataFrame containing JSON data
+df = spark.read.option("multiline", "true").json(f"Files/{start_date}_earthquake_data.json")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# Reshape earthquake data by extracting and renaming key attributes for further analysis.
+df = \
+df.\
+    select(
+        'id',
+        col('geometry.coordinates').getItem(0).alias('longitude'),
+        col('geometry.coordinates').getItem(1).alias('latitude'),
+        col('geometry.coordinates').getItem(2).alias('elevation'),
+        col('properties.title').alias('title'),
+        col('properties.place').alias('place_description'),
+        col('properties.sig').alias('sig'),
+        col('properties.mag').alias('mag'),
+        col('properties.magType').alias('magType'),
+        col('properties.time').alias('time'),
+        col('properties.updated').alias('updated')
+        )
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# Convert 'time' and 'updated' columns from milliseconds to timestamp format for clearer datetime representation.
+df = df.\
+    withColumn('time', col('time')/1000).\
+    withColumn('updated', col('updated')/1000).\
+    withColumn('time', col('time').cast(TimestampType())).\
+    withColumn('updated', col('updated').cast(TimestampType()))
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# appending the data to the gold table
+df.write.mode('append').saveAsTable('earthquake_events_silver')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
